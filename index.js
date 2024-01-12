5aee2dc8e42d63dca16fd5e42b125bc3094a60d5
nasr250 <56081094+nasr250@users.noreply.github.com>
2024-01-13 00:23:53 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Client, GatewayIntentBits } = require('discord.js'); // Merk op dat de correcte import wordt gebruikt
+
+const client = new Client({
+  intents:[
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.MessageContent
+  ],
+});
+
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("messageCreate", async message => {
+  if(message.content === "ping") {
+    message.channel.send("pong")
+  }
+});
+
+client.on("messageCreate", async message => {
+  if(message.content === "hey") {
+    message.channel.send("je stinkt")
+  }
+});
