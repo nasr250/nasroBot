24742a4037bb180aa72662a6949da9c134a9df32
nasr250 <56081094+nasr250@users.noreply.github.com>
2024-01-13 01:17:25 +0100

    mention with "ping"

--- index.js
+++ index.js
@@ -13,15 +13,20 @@ const client = new Client({
 
 
 client.login(process.env.DISCORD_TOKEN);
-
-client.on("messageCreate", async message => {
-  if(message.content === "ping") {
-    message.channel.send("pong")
-  }
+client.once('ready', () => {
+  console.log('Bot is ready!');
 });
 
-client.on("messageCreate", async message => {
-  if(message.content === "aaaaaa") {
-    message.channel.send("je stinkt")
+client.on("messageCreate", async (message) => {
+  // Controleer of het bericht is verzonden door een andere gebruiker dan de bot
+  if (message.author.bot) return;
+
+  // Controleer of de bot wordt getagd en de inhoud van het bericht "ping" is
+  const botMention = message.mentions.has(client.user);
+  const hasPing = message.content.toLowerCase().includes("ping");
+
+  if (botMention && hasPing) {
+    // Reageer op het bericht met "Pong!"
+    message.reply("Pong!");
   }
 });
